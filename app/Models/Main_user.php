931b7a786139da351d0f9a3cdbc02d5ae86bd117
backing <anatolyu128@gmail.com>
2024-04-07 21:09:44 +0500

    code review

--- app/Models/Main_user.php
+++ app/Models/Main_user.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Разделители в названиях классах пишутся без нижних подчеркиваний - MainUser
 class Main_user extends Model
 {
     use HasFactory;
